37d8a5a7b8808df724c73a40f24fbfb38157f27f
Alexander Blomberg <info@dalasoftware.se>
2025-01-05 21:34:05 +0100

    Create the Feed and Post components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import Image from '@/components/Image';
+import Feed from '@/components/Feed';
+import Share from '@/components/Share';
+import Link from 'next/link';
 
 const Homepage = () => {
   return (
     <div className=''>
-      <h1>X Clone</h1>
+      <div className='px-3 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray'>
+        <Link
+          className='pb-3 flex items-center border-b-4 border-iconBlue'
+          href='/'
+        >
+          For you
+        </Link>
+        <Link className='pb-3 flex items-center' href='/'>
+          Following
+        </Link>
+        <Link className='pb-3 flex items-center' href='/'>
+          React.js
+        </Link>
+        <Link className='pb-3 flex items-center' href='/'>
+          Next.js
+        </Link>
+        <Link className='pb-3 flex items-center' href='/'>
+          TypeScript
+        </Link>
+      </div>
+      <Share />
+      <Feed />
     </div>
   );
 };
