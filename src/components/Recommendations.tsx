04cb3a5e2bb101394c749334b020d474b72c7a35
Alexander Blomberg <info@dalasoftware.se>
2025-01-06 15:43:02 +0100

    Create the Recommendations component

--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -1,7 +1,81 @@
-import React from 'react';
+import Link from 'next/link';
+import Image from './Image';
 
 const Recommendations = () => {
-  return <div>Recommendations</div>;
+  return (
+    <div className='p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4'>
+      {/* USER CARD */}
+      <div className='flex items-center justify-between'>
+        {/* IMAGE AND USER INFO */}
+        <div className='flex items-center gap-2'>
+          <div className='relative rounded-full overflow-hidden w-10 h-10'>
+            <Image
+              path='/XC/general/avatar.png'
+              alt='John Doe'
+              width={100}
+              height={100}
+              tr={true}
+            />
+          </div>
+          <div className=''>
+            <h1 className='text-md font-bold'>John Doe</h1>
+            <span className='text-textGray text-sm'>@johnDoe</span>
+          </div>
+        </div>
+        {/* BUTTON */}
+        <button className='py-1 px-4 font-semibold bg-white text-black rounded-full'>
+          Follow
+        </button>
+      </div>
+      <div className='flex items-center justify-between'>
+        {/* IMAGE AND USER INFO */}
+        <div className='flex items-center gap-2'>
+          <div className='relative rounded-full overflow-hidden w-10 h-10'>
+            <Image
+              path='/XC/general/avatar.png'
+              alt='John Carter'
+              width={100}
+              height={100}
+              tr={true}
+            />
+          </div>
+          <div className=''>
+            <h1 className='text-md font-bold'>John Carter</h1>
+            <span className='text-textGray text-sm'>@johnCarter</span>
+          </div>
+        </div>
+        {/* BUTTON */}
+        <button className='py-1 px-4 font-semibold bg-white text-black rounded-full'>
+          Follow
+        </button>
+      </div>
+      <div className='flex items-center justify-between'>
+        {/* IMAGE AND USER INFO */}
+        <div className='flex items-center gap-2'>
+          <div className='relative rounded-full overflow-hidden w-10 h-10'>
+            <Image
+              path='/XC/general/avatar.png'
+              alt='Alexander Blomberg'
+              width={100}
+              height={100}
+              tr={true}
+            />
+          </div>
+          <div className=''>
+            <h1 className='text-md font-bold'>Alexander Blomberg</h1>
+            <span className='text-textGray text-sm'>@alexanderBlomberg</span>
+          </div>
+        </div>
+        {/* BUTTON */}
+        <button className='py-1 px-4 font-semibold bg-white text-black rounded-full'>
+          Follow
+        </button>
+      </div>
+      <Link href='/' className='text-iconBlue'>
+        Show More
+      </Link>
+    </div>
+  );
 };
 
 export default Recommendations;
